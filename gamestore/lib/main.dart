7aa64f620471fa7d4193cc462e8daa17d939edca
caiolara090 <caio.jorge@hotmail.com.br>
2024-03-17 19:49:01 -0300

    Criação_da_pasta_flutter

---
+++ gamestore/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '/login_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginScreen(),
+  ));
+}
