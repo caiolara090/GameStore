f91d002d7c507c4fa27fb7199ded97b60fd0f97f
Lucas-1048 <lucas.almeida1048@gmail.com>
2024-06-08 13:49:51 -0300

    feat: Validação e implementação do login

---
+++ backend/src/adapters/api/controllers/Auth/Login.ts
@@ -0,0 +1,25 @@
+import { StatusCodes } from 'http-status-codes';
+import { Request, Response } from 'express';
+import { sign } from '../../../../utils/Token';
+
+export const login = async (_req: Request, res: Response) => {
+    const user = res.locals.user;
+
+    if (user) {
+        try {
+            const accessToken = sign({ uid: user._id });
+            if(accessToken === 'JWT_SECRET_NOT_FOUND') {
+                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ 
+                    error: 'Internal server error' 
+                });
+            }
+            res.cookie('access_token', accessToken, { httpOnly: true });
+    
+            return res.status(StatusCodes.OK).json({ 
+                message: 'Login successful', info: user
+            });
+        } catch (error: any) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error.message);
+        }
+    }
+};
