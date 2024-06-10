61925465f59ec6452686c007c18ac64901e14565
Luiz Fernando <luizfgrocha04@gmail.com>
2024-06-10 03:03:21 -0300

    feat: Adicionado campo de status na amizade

--- backend/src/adapters/database/models/Friendship.ts
+++ backend/src/adapters/database/models/Friendship.ts
@@ -4,6 +4,15 @@ import { IFriendship } from "../../../domain/entities/Friendship";
 const FriendshipSchema = new mongoose.Schema<IFriendship>({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } as unknown as mongoose.SchemaDefinitionProperty<string>, // Para o TypeScript aceitar string na interface e ObjectId no schema
   friend: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } as unknown as mongoose.SchemaDefinitionProperty<string>,
+  status: { 
+    type: Number, 
+    required: true,
+    enums: [
+      0,    // Solicitação pendente
+      1,    // Solicitação aceita
+      2    // Solicitação enviada
+    ],
+  },
 });
 
 export const FriendshipModel = mongoose.model<IFriendship>('Friendship', FriendshipSchema);
--- backend/src/domain/entities/Friendship.ts
+++ backend/src/domain/entities/Friendship.ts
@@ -1,11 +1,13 @@
 export interface IFriendship {
   user: string;
   friend: string;
+  status: number;
 }
 
 export class Friendship implements IFriendship {
   constructor(
     public user: string,
     public friend: string,
+    public status: number,
   ) {}
 }
