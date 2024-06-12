be9c3ed69f667603cd0a60583eae38146a494221
Thales Fernandes <thales.fernandes47@gmail.com>
2024-06-12 19:46:00 -0300

    Alterando main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gamestore/PaginaBiblioteca.dart';
 import 'package:gamestore/PaginaDados.dart';
 import 'package:gamestore/PaginaLoja.dart';
+import 'package:gamestore/PaginaAmigos.dart';
 import '/PaginaLogin.dart';
+import 'package:gamestore/PaginaCreditos.dart';
 
 
 void main() {
@@ -13,6 +15,9 @@ void main() {
       '/dados': (context) => PaginaDados(),
       '/loja': (context) => PaginaLoja(),
       '/biblioteca':(context) => GameLibraryPage(),
+      '/amigos':(context) => FriendPage(),
+      '/creditos':(context) => AddCreditsScreen()
     },
   ));
 }
+
