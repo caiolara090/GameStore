f0691227de684098572476383b2fc6e283bd29cb
Luiz Fernando <luizfgrocha04@gmail.com>
2024-06-14 18:44:54 -0300

    feat: router de amizade adicionado ao app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import CookieParser from 'cookie-parser';
 import { userRouter } from './adapters/api/routes/User';
+import { friendshipRouter } from './adapters/api/routes/Friendship';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app
   .use(express.json())
   .use(CookieParser())
   .use(userRouter)
+  .use(friendshipRouter);
 
 export default app;
